dd9c0714f85d3fe8c05bfb8f252aa49365855856
WernerWxxx <tsp1rejestr@vp.pl>
2023-04-21 12:21:49 +0200

    Zadanie Domowe Dzień 4

--- ChalengeApp/ChalengeApp/Program.cs
+++ ChalengeApp/ChalengeApp/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Werner Wxxx
+// Zadanie Domowe Dzień 4
+
+string imie = "Ewa";
+string plec = "Kobieta";
+int wiek = 33;
+
+if (plec == "Kobieta")
+{
+    if (wiek < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    else if (wiek >= 33 && imie == "Ewa")
+    {
+        Console.WriteLine("Ewa, lat 33");
+    }
+}
+else if (wiek < 18)
+{
+    Console.WriteLine("Niepelnoletni mezczyzna");
+}
