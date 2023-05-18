de4f09e46ed623e015ea8fdd02ba2987fbfb347f
WernerWxxx <tsp1rejestr@vp.pl>
2023-05-18 23:08:51 +0200

    Zadanie Domowe Dzień 9

---
+++ ChalengeApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using ChalengeApp;
+
+Console.WriteLine("Zadanie Domowe Dzien 9 ");
+Console.WriteLine(" ");
+
+var employee = new Employee("Adam", "Nowicki");
+
+
+employee.AddGrade(2);
+employee.AddGrade(7);
+employee.AddGrade(4); 
+
+
+var Statistics = employee.GetStatistics();
+
+Console.WriteLine($"Min: {Statistics.Min}");
+Console.WriteLine($"Max: {Statistics.Max}");
+Console.WriteLine($"Average: {Statistics.Average:N2}");
+
---
+++ ChallengeApp.Test/EmployeeTest.cs
@@ -0,0 +1,82 @@
+﻿using ChalengeApp;
+
+namespace ChallengeApp.Test
+{
+    public class Test
+    {
+        [Test]  //1
+        public void MaxPointsTest()
+        {
+
+            // arrange
+            var employee = new Employee("Adam", "Nowicki");
+
+            employee.AddGrade(2);
+            employee.AddGrade(7);
+            employee.AddGrade(4);
+
+            // act
+            var statistics = employee.GetStatistics();
+
+            // assert
+            Assert.AreEqual(7, statistics.Max);
+
+        }
+
+        [Test]  //2
+        public void MinPointsTest()
+        {
+            // arrange
+            var employee = new Employee("Adam", "Nowicki");
+            employee.AddGrade(2);
+            employee.AddGrade(7);
+            employee.AddGrade(4);
+
+            // act
+            var statistics = employee.GetStatistics();
+
+            // assert
+            Assert.AreEqual(2, statistics.Min);
+
+        }
+
+        [Test]  // 3
+        public void AveragePointsTest()
+        {
+            // arrange
+            var employee = new Employee("Adam", "Nowicki");
+
+            employee.AddGrade(2);
+            employee.AddGrade(7);
+            employee.AddGrade(4);
+
+            // act
+            var statistics = employee.GetStatistics();
+
+            // assert
+            Assert.AreEqual(Math.Round(4.33, 2), Math.Round(statistics.Average, 2));
+            //Assert.AreEqual(6.67, statistics.Average, 0.01);
+
+        }
+
+        //  [Test]  // 4  - Błąd w Kodzie Programu
+        //public void AverageBladPointsTest()
+        //{
+        // arrange
+        //    var employee = new Employee("Adam", "Nowicki");
+
+        //    employee.AddGrade(1);
+        //    employee.AddGrade(7);
+        //    employee.AddGrade(3);
+
+        // act
+        //   var statistics = employee.GetStatistics();
+
+        // assert
+        //   Assert.AreEqual(Math.Round(4.33, 2), Math.Round(statistics.Average, 2));
+
+
+        // }
+
+    }
+}
